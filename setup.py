d945ca143162047658b0a2ff04f736ff97c09f95
Zachary Cutlip <uid000@gmail.com>
2019-07-30 10:25:52 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+about = {}
+with open("withtimer/__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      url="TBD",
+      packages=['withtimer'],
+      python_requires='>=3.7',
+      install_requires=[],
+      package_data={'withtimer': ['config/*']},
+      )
