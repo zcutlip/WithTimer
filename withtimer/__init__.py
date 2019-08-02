c1ab0b59ca80edef16a8cace8a8df5f9bd8221ec
Zachary Cutlip <uid000@gmail.com>
2019-08-02 12:14:29 -0700

    export Timer class in __init__.__all__

--- withtimer/__init__.py
+++ withtimer/__init__.py
@@ -1,11 +1,13 @@
-from withtimer.__about__ import (
+from .__about__ import (
     __version__,
     __title__,
     __summary__
 )
+from .withtimer import Timer
 
 __all__ = [
     "__version__",
     "__title__",
-    "__summary__"
+    "__summary__",
+    "Timer"
 ]
