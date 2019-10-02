5f0f1bf4958a7ea3549fdb7fb017765955cfee10
Zachary Cutlip <uid000@gmail.com>
2019-10-02 15:45:34 -0700

    flesh out setup.py metadata

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ about = {}
 with open("withtimer/__about__.py") as fp:
     exec(fp.read(), about)
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
 setup(name=about["__title__"],
       version=about["__version__"],
       description=about["__summary__"],
+      long_description=long_description,
+      license="MIT",
+      author="Zachary Cutlip",
+      long_description_content_type="text/markdown",
       url="TBD",
       packages=['withtimer'],
       python_requires='>=2.7',
