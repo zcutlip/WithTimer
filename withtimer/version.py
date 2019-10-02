5c8d412fba446b508f54967528c00cadb5bb4514
Zachary Cutlip <uid000@gmail.com>
2019-10-02 15:26:19 -0700

    added class documentation to WithTimerAbout

--- withtimer/version.py
+++ withtimer/version.py
@@ -6,6 +6,11 @@ from . import (
 
 
 class WithtimerAbout:
+    """
+    If you need to print the version of WithTimer for some reason,
+    instantiate one of these and str() it.
 
+    print(WithTimerAbout())
+    """
     def __str__(self):
         return "%s %s version %s" % (__title__, __summary__, __version__)
